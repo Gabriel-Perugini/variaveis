36ded1e80dccc54fa3b2f829459796a4037daf5b
Gabriel-Perugini <123771750+Gabriel-Perugini@users.noreply.github.com>
2023-08-02 11:17:05 -0300

    atualização 1

---
+++ 12, 13, 14, 15, 16, 17, 18 e 19.js
@@ -0,0 +1,25 @@
+let PowerUp = 1
+PowerUp += 5
+// separação
+let PowerLevel = 101
+PowerLevel -= 100
+// separação
+let MultiplyMe = 10
+MultiplyMe *= 11
+// separção
+let quarterMe = 16
+quarterMe /= 4
+// separação
+let gainedDollar = 1
+gainedDollar++
+// separação
+let lostDollar = 1
+lostDollar--
+// separação
+const myName = "Gabriel";
+const myCity = "New York";
+const myMessage = `My name is ${myName}. My favorite city is ${myCity}.`;
+console.log(myMessage);
+
+let newVariable = "Ai Painho!";
+console.log(typeof newVariable);
